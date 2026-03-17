537b29f067d14353998976b782f775359a0e22c8
Nels Martin <nels@nelsmartin.com>
2026-03-17 16:48:31 -0700

    removes sorries

--- LeanStuff/SimpleTheorems.lean
+++ LeanStuff/SimpleTheorems.lean
@@ -1,12 +1,11 @@
 theorem add_zero (n : Nat) :  n + 0 = n :=
-  sorry
+
 theorem add_assoc (m n k : Nat) : n + m + k = n + (m + k) :=
-  sorry
+
 
 theorem zero_add (n : Nat): 0 + n = n :=
-  sorry
+
 theorem succ_add (n m : Nat) : n + 1 + m = (n + m) + 1 :=
-  sorry
+
 
 theorem add_succ (n m : Nat) : n + m + 1 = (n + m) + 1 :=
-  sorry
