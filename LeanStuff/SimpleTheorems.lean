6fd1652f49b8387dcfb0b1990dfc30c273d905a9
Nels Martin <nels@nelsmartin.com>
2026-03-17 16:53:13 -0700

    added file

--- LeanStuff/SimpleTheorems.lean
+++ LeanStuff/SimpleTheorems.lean
@@ -1,11 +1,16 @@
-theorem add_zero (n : Nat) :  n + 0 = n :=
+theorem add_zero (n : Nat) :  n + 0 = n := by
+  sorry
 
-theorem add_assoc (m n k : Nat) : n + m + k = n + (m + k) :=
-
-
-theorem zero_add (n : Nat): 0 + n = n :=
-
-theorem succ_add (n m : Nat) : n + 1 + m = (n + m) + 1 :=
+theorem add_assoc (m n k : Nat) : n + m + k = n + (m + k) := by
+  sorry
 
 
-theorem add_succ (n m : Nat) : n + m + 1 = (n + m) + 1 :=
+theorem zero_add (n : Nat): 0 + n = n := by
+  sorry
+
+theorem succ_add (n m : Nat) : n + 1 + m = (n + m) + 1 := by
+  sorry
+
+
+theorem add_succ (n m : Nat) : n + m + 1 = (n + m) + 1 := by
+  sorry
